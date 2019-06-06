039c8d8da6e03a014f3ea32acdca0309090cb110
Chris Wegscheid <cwegscheid08@outlook.com>
2019-06-06 14:20:41 -0400

    working through footer alignment

--- Gemfile
+++ Gemfile
@@ -38,9 +38,12 @@ gem 'sitemap_generator'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  # Seeds the database
+  gem 'faker', '1.7.3'
 end
 
 group :development do
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,7 +8,8 @@ class MembersController < ApplicationController
 	def create
 		@member = Member.create(member_params)
 		if @member != false
-			render 'index'
+			flash[:success] = "Congrats #{@member.name}, you have created your account."
+			render 'show'
 		else
 			render 'new'
 		end
@@ -20,9 +21,26 @@ class MembersController < ApplicationController
 
 	def show
 		@member = Member.find(params[:id])
-
 	end
 
+	def edit
+		@member = Member.find(params[:id])
+	end
+
+	def update
+		@member = Member.find(params[:id])
+		if @member.update_attributes(member_params)
+			redirect_to @member
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		Member.find(params[:id]).destroy
+		flash[:delete] = "You have successfully deleted your account."
+		redirect_to root_url
+	end
 
 
 	private
