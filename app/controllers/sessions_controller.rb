474adfd136f383b838da6e5fa7cabbdcfa3bb54b
Chris Wegscheid <cwegscheid08@outlook.com>
2019-06-10 00:04:35 -0400

    generated a mailer

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		member = Member.find_by(email: params[:session][:email].downcase)
+		if member && member.authenticate(params[:session][:password])
+			
+		else
+			flash.now[:danger] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+
+	def delete
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get '/about', to: 'static_pages#about'
   get '/services', to: 'static_pages#services'
   get '/contact', to: 'static_pages#contact'
