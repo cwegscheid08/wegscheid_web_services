adc93da6675d38bba263b6106556b84c34bd569b
Chris Wegscheid <cwegscheid08@outlook.com>
2020-01-08 16:29:34 -0500

    1/8/2020

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  # root 'static_pages#home'
+  root 'static_pages#construction'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -10,5 +11,6 @@ Rails.application.routes.draw do
   get '/log_cabin', to: 'static_pages#log_cabin'
   get '/news', to: 'static_pages#news'
   get '/portfolio', to: 'static_pages#portfolio'
+  get '/construction', to: 'static_pages#construction'
   resources :members
 end
